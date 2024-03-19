33d9f6ed55626c0285584442bd1c90baba3b66a8
master-dhanesh <dhanesh1296@gmail.com>
2024-03-19 10:08:58 +0530

    plus mouse follower

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const heart = document.querySelector(".heart");
 const card = document.querySelector(".card");
+const heart = document.querySelector(".heart");
 
 card.addEventListener("dblclick", function () {
-    heart.style.opacity = 1;
     heart.style.scale = 1;
+    heart.style.opacity = 1;
+
     setTimeout(() => {
-        heart.style.opacity = 0;
         heart.style.scale = 0;
+        heart.style.opacity = 0;
     }, 350);
+});
+
+const body = document.querySelector("body");
+const cursor = document.querySelector(".cursor");
+
+body.addEventListener("mousemove", function (e) {
+    cursor.style.left = `${e.x + 10}px`;
+    cursor.style.top = `${e.y + 10}px`;
+});
+
+card.addEventListener("mouseenter", function (e) {
+    cursor.style.scale = 3;
+});
+card.addEventListener("mouseleave", function (e) {
+    cursor.style.scale = 1;
 });
